61b9d328bbfb0841fec3a9e38f564562daed7067
alex v <alex@encrypt-s.com>
2021-09-06 19:53:15 +0200

    twitter login

--- src/components/Sign.tsx
+++ src/components/Sign.tsx
@@ -1,16 +1,110 @@
 import * as React from "react";
-import styled from '@emotion/styled'
-import QRCode from "react-qr-code";
-import Box from "@material-ui/core/Box"
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import {themeOptions} from "../App";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import {CardHeader} from "@material-ui/core";
 import TelegramLoginButton from "./TelegramLogin";
-import TwitterLogin from "react-twitter-auth";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import queryString from 'query-string';
+
+const apiPath = '/api';
+
+function TwitterLogin() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [name, setName] = useState();
+    const [imageUrl, setImageUrl] = useState();
+    const [status, setStatus] = useState();
+    const [url, setUrl] = useState();
+
+    const login = () => {
+        (async () => {
+
+            try {
+                //OAuth Step 1
+                const response = await axios({
+                    url: `${apiPath}/twitter/oauth/request_token`,
+                    method: 'POST'
+                });
+
+                const { oauth_token } = response.data;
+                //Oauth Step 2
+                window.location.href = `https://api.twitter.com/oauth/authenticate?oauth_token=${oauth_token}`;
+            } catch (error) {
+                console.error(error);
+            }
+
+        })();
+    }
+
+    const logout = () => {
+        (async () => {
+            try {
+                await axios({
+                    url: `${apiPath}/twitter/logout`,
+                    method: 'POST'
+                });
+                setIsLoggedIn(false);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }
+
+    useEffect(() => {
+        (async() => {
+
+            const {oauth_token, oauth_verifier} = queryString.parse(window.location.search);
+
+            if (oauth_token && oauth_verifier) {
+                try {
+                    //Oauth Step 3
+                    await axios({
+                        url: `${apiPath}/twitter/oauth/access_token`,
+                        method: 'POST',
+                        data: {oauth_token, oauth_verifier}
+                    });
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+
+            try {
+                //Authenticated Resource Access
+                const {data: {name, profile_image_url_https, status, entities}} = await axios({
+                    url: `${apiPath}/twitter/users/profile_banner`,
+                    method: 'GET'
+                });
+
+                setIsLoggedIn(true);
+                setName(name);
+                setImageUrl(profile_image_url_https);
+                setStatus(status.text);
+                setUrl(entities.url.urls[0].expanded_url);
+            } catch (error) {
+                console.error(error);
+            }
+
+
+        })();
+    }, []);
+
+    return !isLoggedIn ? (
+        <Button onClick={login}>Connect with Twitter</Button>
+    ): (
+        <div>
+            <div><img alt='User profile' src={imageUrl}/></div>
+            <div>Name: {name}</div>
+            <div>URL: {url}</div>
+            <div>Status: {status}</div>
+            <button className='signout-btn' onClick={logout}>Sign Out</button>
+        </div>
+    );
+}
+
+
 const Sign = (props: any) => {
     const {address, chainId, queryParams, action} = props;
 
@@ -58,18 +152,7 @@ Your Navcoin Bridge Team.`;
                     console.log(response);
                 }} botName={"NAV_Airdrop_Bot"} />
 
-                <TwitterLogin
-                    loginUrl="https://airdrop.nav.community/api/v1/auth/twitter"
-                    onFailure={(error: any) => {
-                        alert(error);
-                    }}
-                    onSuccess={(response: any) => {
-                        response.json().then((body: any) => {
-                            alert(JSON.stringify(body));
-                        });
-                    }}
-                    requestTokenUrl="https://airdrop.nav.community/api/v1/auth/twitter/reverse"
-                />
+                <TwitterLogin/>
 
             </CardContent>
         </Card>
